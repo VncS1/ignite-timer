9d2175e6303321a9e45878acfbcbfa034bdc1b18
Vinicius M <viniciusmachioni79@gmail.com>
2022-09-26 10:15:22 -0300

    Alterando tempo minimo de um ciclo

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -38,7 +38,7 @@ export function NewCycleForm() {
                 placeholder="00"
                 disabled={!!activeCycle}
                 step={5}
-                min={1}
+                min={0}
                 max={60}
                 {...register('minutesAmount', { valueAsNumber: true })}//falando que esse input tem que passar os dados como number
             />
